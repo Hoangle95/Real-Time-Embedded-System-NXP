276c25c3241273a81c46bf1313b1075b3e78a588
Hoang Le <v.hoang3598@gmail.com>
2020-11-11 20:15:13 -0800

    test

---
+++ sjtwo-c-master/projects/lpc40xx_freertos/l5_application/main_OLED.c
@@ -0,0 +1,41 @@
+#include "FreeRTOS.h"
+#include "adc.h"
+#include "board_io.h"
+#include "common_macros.h"
+#include "delay.h"
+#include "gpio.h"
+
+#include "gpio_lab.h"
+#include "lpc40xx.h"
+#include "lpc_peripherals.h"
+#include "periodic_scheduler.h"
+
+#include "oled.h"
+#include "queue.h"
+#include "semphr.h"
+#include "sj2_cli.h"
+#include "ssp2_lab.h"
+#include "task.h"
+#include <stdio.h>
+
+#include "string.h"
+#include "uart_lab.h"
+
+void oled_task() {
+  while (1) {
+    turn_on_lcd();
+  }
+}
+
+int main(void) {
+
+  puts("Starting RTOS\n");
+  // turn_on_lcd();
+  oled_print("Hoang");
+
+  while (1) {
+  }
+  // vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
+
+  return 0;
+}
