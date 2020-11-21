53899873dd969c91cd809bcbe7fb1a6d9bff39b8
Hoang Le <v.hoang3598@gmail.com>
2020-11-21 15:43:04 -0800

    Play_next and play_previoys DONE

---
+++ sjtwo-c-master/projects/DecoderV3/README.md
@@ -0,0 +1,30 @@
+## MP3 Project: Develop VS1053b (SPI) MIDI Audio Codec Circuit
+
+![image](https://user-images.githubusercontent.com/38081550/97953088-5e363000-1d54-11eb-8272-0d227433ceaf.png)
+
+### OBJECTIVE
+
+1. Create TaskHandle_t "player_handle" to Suspend(pause) or Resume(resume) a song
+2. Link the "&player_handle" to the last parameter of mp3_player_task to pause_resume_task
+3. Create "pause_resume" binary semaphore to trigger signal
+4. Create pause_resume_ISR and attach it to function enbale_interrupt() : review gpio_isr.c
+5. Create pause_resume_Button task to decide when to suspend and when to resume
+   a) SemaphoreGiveFromISR(pause_resume) in pause_resume_ISR task
+   b) SemaphoreTake(pause_resume) in pause_resume_Button task
+   i) When (pause) is true, suspend task then set pause to false
+   ii) When hit pause again (false value), it runs the else case and resume the task
+
+- L3-Driver [Decoder Header](https://github.com/Hoangle95/Real-Time-Embedded-System-NXP/blob/main/sjtwo-c-master/projects/Decoder/l3_drivers/decoder_mp3.h)
+
+- L3-Driver [Decoder Source](https://github.com/Hoangle95/Real-Time-Embedded-System-NXP/blob/main/sjtwo-c-master/projects/Decoder/l3_drivers/sources/decoder_mp3.c)
+- L5-Application [main](https://github.com/Hoangle95/Real-Time-Embedded-System-NXP/blob/main/sjtwo-c-master/projects/Decoder/l5_application/main.c)
+- L5-Application [song_handler.c](https://github.com/Hoangle95/Real-Time-Embedded-System-NXP/blob/main/sjtwo-c-master/projects/DecoderV1/l5_application/song_handler.c)
+- L5-Application [song_handler.h](https://github.com/Hoangle95/Real-Time-Embedded-System-NXP/blob/main/sjtwo-c-master/projects/DecoderV1/l5_application/song_handler.h)
+
+```
+# Compile Command:
+scons --project=mp3_decoder
+
+# SJ2 Board Flash Command:
+python nxp-programmer/flash.py --input _build_mp3_decoder/mp3_decoder.bin
+```
---
+++ sjtwo-c-master/projects/DecoderV3/l3_drivers/gpio_isr.h
@@ -0,0 +1,46 @@
+// @file gpio_isr.h
+// @file gpio_isr.h
+#pragma once
+
+#include "lpc40xx.h"
+#include <stdint.h>
+#include <stdio.h>
+
+typedef enum {
+  GPIO_INTR__FALLING_EDGE, // 0
+  GPIO_INTR__RISING_EDGE,  // 1
+} gpio_interrupt_e;
+
+typedef struct {
+  uint8_t port;
+  uint8_t pin;
+} port_pin_s;
+
+// Function pointer type (demonstrated later in the code sample)
+typedef void (*function_pointer_t)(void);
+/***********************************************************************************************
+        Two main things ( Save the CallBack_function to INTR table + Config INTR)
+************************************************************************************************/
+
+/* Allow the user to setup + attach their callbacks function Port 0 */
+void gpio0__attach_interrupt(uint8_t pin_num, gpio_interrupt_e interrupt_type, function_pointer_t callback);
+/* Allow the user to setup + attach their callbacks function Port 2 */
+void gpio2__attach_interrupt(uint8_t pin_num, gpio_interrupt_e interrupt_type, function_pointer_t callback);
+
+/***********************************************************************************************
+        Two main things ( check the interrupt Pin + Map it to interrupt vector table in ISR)
+        Our main() should configure interrupts to invoke this dispatcher
+        You can hijack 'interrupt_vector_table.c' or use API at lpc_peripherals.h
+************************************************************************************************/
+
+// Dispatch the specific function in Interrupt Service Routine (ISR)--Port 0
+void gpio0__interrupt_dispatcher(void);
+
+// Dispatch the specific function in Interrupt Service Routine (ISR)--Port 2
+void gpio2__interrupt_dispatcher(void);
+
+// An unregistered interrupt handler is a forever loop
+static void lpc_peripheral__halt_handler(void) {
+  while (1) { /* Nothing*/
+  }
+}
